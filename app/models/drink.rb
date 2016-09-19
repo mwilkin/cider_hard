e95e2a175dcb360f858695256084422e0692c87f
Matt Wilkin <mwilkin22@gmail.com>
2016-09-19 15:07:28 -0700

    update unit testing: change type => fruit.

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,6 +1,6 @@
 class Drink < ActiveRecord::Base
   validates :name, :presence => true
-  validates :type, :presence => true
+  validates :fruit, :presence => true
   validates :origin, :presence => true
   validates :cost, :presence => true
 end
--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Drink do
   it { should validate_presence_of :name}
-  it { should validate_presence_of :type}
+  it { should validate_presence_of :fruit}
   it { should validate_presence_of :origin}
   it { should validate_presence_of :cost}
 
