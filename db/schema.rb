2506652700884ab2386a73e075a425cf0259b088
Matt Wilkin <mwilkin22@gmail.com>
2016-09-19 15:04:52 -0700

    change column name in Drinks.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919212037) do
+ActiveRecord::Schema.define(version: 20160919220327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "fruit"
     t.string   "origin"
     t.integer  "cost"
     t.datetime "created_at", null: false
