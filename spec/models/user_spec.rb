3d9d044c4fba1833453c319bfcd0a9487231ac46
Matt Wilkin <mwilkin22@gmail.com>
2016-09-26 05:46:40 -0700

    create Factory for User:email.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  
+describe User do
+  # it "sends an email when the user is created" do
+  #   user = FactoryGirl.create(:user)
+  #   ActionMailer::Base.deliveries.last.to.should eq [user.email]
+  # end
 end
